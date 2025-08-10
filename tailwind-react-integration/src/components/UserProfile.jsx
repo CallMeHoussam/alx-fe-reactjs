13e8b50cfb20ed72bfda7243756150a815900825
Evoless <amoogames519@gmail.com>
2025-08-10 22:42:51 +0100

    tailwind-react-integration

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,8 @@
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto rounded-lg shadow-lg text 
+    hover:shadow-xl">
 
       <img 
         src="https://via.placeholder.com/150" 
@@ -14,6 +16,7 @@ function UserProfile() {
       <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
+    </div>
     </div>
   );
 }
