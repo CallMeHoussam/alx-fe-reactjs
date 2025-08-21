9d08281c127aec8029164a1d5bf4219a5a2e69d5
Evoless <amoogames519@gmail.com>
2025-08-21 14:35:44 +0100

    Advanced Routing in React with React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,8 +4,6 @@ import About from './components/About';
 import Blog from './components/Blog';
 import BlogPost from './components/BlogPost';
 import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
 import ProtectedRoute from './components/ProtectedRoute';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
@@ -20,17 +18,13 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:postId" element={<BlogPost />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={
             <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
-          }>
-            <Route index element={<ProfileDetails />} />
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
+          } />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
