3723594b3db92952b5df713725aff0eda02551cf
Evoless <amoogames519@gmail.com>
2025-08-21 23:46:02 +0100

    Implementing and Testing a Todo List Component in React

--- react-todo/vite.config.js
+++ react-todo/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    setupFiles: ['./src/setupTests.js'],
     coverage: {
-      provider: 'istanbul'
-    }
-  }
-})
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+});
